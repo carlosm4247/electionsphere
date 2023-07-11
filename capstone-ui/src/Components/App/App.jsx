f9050ecb2df77da6f4b10a2ac8afe41786646260
Carlos Moreno Serrano <carlosmoreno@meta.com>
2023-07-11 11:13:39 -0700

    Set up basic UI for Signup and Login

--- capstone-ui/src/Components/App/App.jsx
+++ capstone-ui/src/Components/App/App.jsx
@@ -1,29 +1,64 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { UserContext } from '../../UserContext';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import LoginForm from '../LoginForm/LoginForm';
+import SignupForm from '../SignupForm/SignupForm';
 import "./App.css";
 
 export default function App() {
+
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  })
+
+  const updateUser = (newUser) => {
+    setUser(newUser)
+  }
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user])
+
+
+
+  const handleLogout = () => {
+    updateUser(null);
+  };
+
   return (
     <div className='app'>
-      <BrowserRouter>
-        <main>
-          <div className="navbar">
-            <div className="dropdown">
-              <p>Dropdown Placeholder</p>
+      <UserContext.Provider value={{ user, updateUser }}>
+        <BrowserRouter>
+          <main>
+            <div className="navbar">
+              <div className="dropdown">
+                <Link to="/"><button>Home</button></Link>
+              </div>
+              <h2 className="title">Website Name</h2>
+              <div className='user-info'>
+                {user ? (
+                  <>
+                  <span>Welcome, {user.username}! |</span>
+                  <button onClick={handleLogout}>Logout</button>
+                  </>
+                ) : (
+                  <Link to="/login"><button>Login</button></Link>
+                )}
+              </div>
             </div>
-            <h2 className="title">Website Name</h2>
-            <button className="button">Profile Placeholder</button>
-          </div>
-          <div className="content">
-            <Routes>
-              <Route path='/' element={
-                                <p>Message</p>
-                                }/>
-            </Routes>
-            
-          </div>
-        </main>
-      </BrowserRouter>
+            <div className="content">
+              <Routes>
+                <Route path='/' element={<p>Message</p>}/>
+                <Route path='/login' element={<LoginForm />}/>
+                <Route path='/signup' element={<SignupForm />}/>
+              </Routes>
+
+            </div>
+          </main>
+        </BrowserRouter>
+      </UserContext.Provider>
+      
     </div>
   );
 }
