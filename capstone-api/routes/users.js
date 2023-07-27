1238e10fd4ab9da666d5379ef732ff4a1c9e07cd
Carlos Moreno Serrano <carlosmoreno@meta.com>
2023-07-26 23:10:04 -0700

    Set up routes for the following/unfollowing post requests, which connect to the database and store the updates to the "following" array.

--- capstone-api/routes/users.js
+++ capstone-api/routes/users.js
@@ -55,4 +55,56 @@ router.post("/users/login", async (req, res) => {
     }
 });
 
+router.post("/follow", async (req, res) => {
+    const { username, candidateName } = req.body;
+
+    try {
+        const user = await User.findOne({ where: { username } });
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        if (user.following.includes(candidateName)) {
+            return res.status(400).json({ error: "Candidate already followed" });
+        }
+
+        const updatedFollowing = [...user.following, candidateName];
+        user.setDataValue('following', updatedFollowing);
+        await user.save();
+
+        res.json({ message: "Candidate followed successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+router.post("/unfollow", async (req, res) => {
+    const { username, candidateName } = req.body;
+
+    try {
+
+        const user = await User.findOne({ where: { username } });
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const index = user.following.indexOf(candidateName);
+        if (index === -1) {
+            return res.status(400).json({ error: "Candidate not followed" });
+        }
+
+        const updatedFollowing = user.following.filter(name => name !== candidateName);
+        user.setDataValue('following', updatedFollowing);
+        await user.save();
+
+        res.json({ message: "Candidate unfollowed successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
 export default router
