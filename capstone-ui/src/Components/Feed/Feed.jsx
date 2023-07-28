315f6ed33fc308bb21251680c2693cefdd71efa6
Carlos Moreno Serrano <carlosmoreno@meta.com>
2023-07-28 12:27:28 -0700

    Added a fetch which sends a request to the /news route based on a search query and a logged-in state to eventually articles from an external api and display their titles in a list.

--- capstone-ui/src/Components/Feed/Feed.jsx
+++ capstone-ui/src/Components/Feed/Feed.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Feed.css"
 
 export default function Feed ( { loggedin }) {
 
+    const [articles, setArticles] = useState([]);
+    const [searchQuery, setSearchQuery] = useState([]);
+
+    useEffect(() => {
+        const fetchNewsArticles = async () => {
+          try {
+            const url = `http://localhost:3001/news?query=${JSON.stringify(searchQuery)}&loggedIn=${loggedin}`;
+    
+            const response = await fetch(url, {
+                method: "GET",
+                credentials: "include",
+            });
+
+            const data = await response.json();
+    
+            setArticles(data.articles);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+    
+        fetchNewsArticles();
+      }, [searchQuery, loggedin]);
+
     return (
         <div className="feed-container">
-            { loggedin ? (
-                <h3>Logged in Feed</h3>
-            ) : (
-                <h3>Not Logged in Feed</h3>
-            )}
+            <h3>Feed</h3>
+            <ul>
+                {articles.map((article, index) => (
+                <li key={index}>
+                    <a href={article.url} target="_blank">
+                    {article.title}
+                    </a>
+                </li>
+                ))}
+            </ul>
         </div>
     )
 }
