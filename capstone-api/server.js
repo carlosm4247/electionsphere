3b8c7f6b35f2463427058e55482bfb8aee808546
Carlos Moreno Serrano <carlosmoreno@meta.com>
2023-07-28 12:17:54 -0700

    Created get route for the news api search, which takes in parameters from the request from the feed to create a request to the news api based on whether a user is signed up or not, and returns the articles.

--- capstone-api/server.js
+++ capstone-api/server.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import { sequelize } from "./database.js";
 import { User } from "./models/users.js";
 import userRoutes from "./routes/users.js";
+import feedRoute from "./routes/feed.js"
 import SequelizeStoreInit from "connect-session-sequelize";
 
 const app = express();
@@ -37,6 +38,7 @@ app.use(
 
 sessionStore.sync();
 
+app.use(feedRoute);
 app.use(userRoutes);
 
 sequelize.sync({ alter: true })
