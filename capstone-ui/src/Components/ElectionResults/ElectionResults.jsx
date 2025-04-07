70304b4f5fd52d8e6959c877ffe177c66174cded
carlosm4247 <carlosm4247@gmail.com>
2025-04-07 13:39:14 -0400

    Adding basic RaceTypeDropdown component for the dropdown which will decide which federal race results are shown.

--- capstone-ui/src/Components/ElectionResults/ElectionResults.jsx
+++ capstone-ui/src/Components/ElectionResults/ElectionResults.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect, useContext, useRef } from "react";
 import { UserContext } from "../../UserContext";
 import PresidentialRaces from "../PresidentialRaces/PresidentialRaces"
+import RaceTypeDropdown from "../RaceTypeDropdown/RaceTypeDropdown";
 
 export default function ElectionResults({selectedCandidates, setSelectedCandidates}) {
-    return (<PresidentialRaces 
-        selectedCandidates={selectedCandidates} 
-        setSelectedCandidates={setSelectedCandidates}
-      />);
+    return (<div className="election results">
+                <RaceTypeDropdown />
+                <PresidentialRaces selectedCandidates={selectedCandidates} setSelectedCandidates={setSelectedCandidates}/>
+            </div>
+            );
 }
---
+++ capstone-ui/src/Components/RaceTypeDropdown/RaceTypeDropdown.jsx
@@ -0,0 +1,5 @@
+import { useState, useEffect, useContext, useRef } from "react";
+
+export default function RaceTypeDropdown() {
+    return (<div>Race Type Dropdown</div>);
+}
