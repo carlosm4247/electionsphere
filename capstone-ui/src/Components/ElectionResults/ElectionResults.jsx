230e052cbcb57708f5fcad6fbe560758473f101a
carlosm4247 <carlosm4247@gmail.com>
2025-04-07 13:20:11 -0400

    Adding blank ElectionResults component for election results page.

---
+++ capstone-ui/src/Components/ElectionResults/ElectionResults.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect, useContext, useRef } from "react";
+import { UserContext } from "../../UserContext";
+
+export default function ElectionResults() {
+    return (<div>Election Results</div>);
+}
